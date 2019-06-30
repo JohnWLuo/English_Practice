ca394c4823b7023d1ff3174e483abe3a718bcdcf
John <john46luo@gmail.com>
2019-06-30 17:02:35 -0400

    Twilio Room Update
    Updated the code so now the app can Connect to twilio's video room service

--- app/src/main/java/com/english/practice/MainActivity.java
+++ app/src/main/java/com/english/practice/MainActivity.java
@@ -3,61 +3,79 @@ package com.english.practice;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.media.AudioManager;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.twilio.video.CameraCapturer;
+import com.twilio.video.ConnectOptions;
 import com.twilio.video.LocalAudioTrack;
 import com.twilio.video.LocalVideoTrack;
+import com.twilio.video.OpusCodec;
+import com.twilio.video.RemoteParticipant;
+import com.twilio.video.RemoteVideoTrackPublication;
+import com.twilio.video.TwilioException;
+import com.twilio.video.Video;
 import com.twilio.video.VideoTextureView;
+import com.twilio.video.Room;
+import com.twilio.video.LocalParticipant;
+
+
+import com.twilio.video.AudioCodec;
+import com.twilio.video.VideoCodec;
+
+import com.koushikdutta.ion.Ion;
+import com.twilio.video.VideoTrack;
+import com.twilio.video.Vp8Codec;
+import com.twilio.video.EncodingParameters;
 
 
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
-import android.hardware.*;
 
-import org.json.JSONObject;
 
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import java.util.Collections;
 
 import static android.view.View.GONE;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String mAccessToken;
-    private static final String TAG = MainActivity.class.getName();
+    private String accessToken;
+    private Room room;
+
     /*
      * Android application UI elements
      */
-    private FrameLayout previewFrameLayout;
-    private ViewGroup localContainer;
-    private ViewGroup participantContainer;
-    private FloatingActionButton callActionFab;
-    private OkHttpClient client = new OkHttpClient();
 
+    private AudioManager audioManager;
     private Context mContext;
     private LocalAudioTrack localAudioTrack;
     private CameraCapturer cameraCapturerCompat;
     private LocalVideoTrack localVideoTrack;
+    private LocalParticipant localParticipant;
+    private TextView videoStatusTextView;
+    private String remoteParticipantIdentity;
+    private boolean disconnectedFromOnDestroy;
+
+    private AudioCodec audioCodec;
+    private VideoCodec videoCodec;
+    private EncodingParameters encodingParameters;
+    private int previousAudioMode;
+    private static final String TAG = "English Practice";
+
 
     private VideoTextureView localVideoTextureView;
+    private boolean isSpeakerPhoneEnabled = true;
 
 
     @Override
@@ -65,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
         //super refers to the immediate parents onCreate method
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
+        videoStatusTextView = findViewById(R.id.videoStatusText);
 
         mContext = this.getApplicationContext();
         /*
@@ -76,6 +95,16 @@ public class MainActivity extends AppCompatActivity {
 
         initializeVideoTextureViews();
         createAudioAndVideoTracks();
+
+        setVolumeControlStream(AudioManager.STREAM_VOICE_CALL);
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        audioManager.setSpeakerphoneOn(isSpeakerPhoneEnabled);
+
+        audioCodec = getAudioCodec();
+        videoCodec = getVideoCodec();
+
+        retrieveAccessTokenfromServer();
+
     }
 
     private boolean checkPermissionForCameraAndMicrophone() {
@@ -113,64 +142,139 @@ public class MainActivity extends AppCompatActivity {
         localVideoTextureView = findViewById(R.id.localContainer);
         localVideoTextureView.setMirror(true);
 
-
-
         VideoTextureView remoteParticipantVideoTextureView = findViewById(R.id.participantContainer);
         remoteParticipantVideoTextureView.setVisibility(GONE);
+
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-    /*
-    private void getCapabilityToken() {
-        try {
-            run("http://[your-ngrok-url].ngrok.io/token", new Callback() {
-
-                @Override
-                public void onFailure(Call call, IOException e) {
-                    e.printStackTrace();
-                }
-
-                @Override
-                public void onResponse(Call call, Response response) throws IOException {
-                    try {
-                        String token = response.body().string();
-                        JSONObject obj = new JSONObject(token);
-                        mAccessToken = obj.getString("token");
-                        Log.d(TAG, token);
-                        initializeTwilioSdk(mAccessToken);
-
-                    } catch (Exception e) {
-                        e.printStackTrace();
+    private void retrieveAccessTokenfromServer() {
+        Ion.with(this)
+                .load(String.format("https://byzantine-hippopotamus-3086.twil.io/AccessToken?identity=%s", "John"))
+                .asString().setCallback((e, token) -> {
+                    if (e == null) {
+                        this.accessToken = token;
+                        /*Toast.makeText(this,
+                               "This is access token " + this.accessToken, Toast.LENGTH_LONG)
+                                .show();*/
+                        connectToRoom("TestRoom");
+                    } else {
+                        Toast.makeText(this,
+                               "Error Retrieving Access Token", Toast.LENGTH_LONG)
+                                .show();
                     }
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
+                });
+    }
+
+    private void connectToRoom(String roomName) {
+        //configureAudio(true);
+        ConnectOptions.Builder connectOptionsBuilder = new ConnectOptions.Builder(accessToken)
+                .roomName(roomName);
+
+        /*
+         * Add local audio track to connect options to share with participants.
+         */
+        if (localAudioTrack != null) {
+            connectOptionsBuilder
+                    .audioTracks(Collections.singletonList(localAudioTrack));
         }
-    }*/
 
-    /*private Call run(String url, Callback callback) throws IOException {
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
+        /*
+         * Add local video track to connect options to share with participants.
+         */
+        if (localVideoTrack != null) {
+            connectOptionsBuilder.videoTracks(Collections.singletonList(localVideoTrack));
+        }
 
-        Call response = client.newCall(request);
-        response.enqueue(callback);
-        return response;
-    }*/
+        /*
+         * Set the preferred audio and video codec for media.
+         */
+        connectOptionsBuilder.preferAudioCodecs(Collections.singletonList(audioCodec));
+        connectOptionsBuilder.preferVideoCodecs(Collections.singletonList(videoCodec));
 
+        /*
+         * Set the sender side encoding parameters.
+         */
+        connectOptionsBuilder.encodingParameters(encodingParameters);
 
+        room = Video.connect(this, connectOptionsBuilder.build(), roomListener());
+        //setDisconnectAction();
+    }
+
+    public AudioCodec getAudioCodec() {
+        return new OpusCodec();
+    }
+
+    public VideoCodec getVideoCodec() {
+        return new Vp8Codec();
+    }
+
+    private Room.Listener roomListener() {
+        return new Room.Listener() {
+            @Override
+            public void onConnected(Room room) {
+                localParticipant = room.getLocalParticipant();
+                videoStatusTextView.setText(String.format("Connected to %s", room.getName()));
+                setTitle(room.getName());
+            }
+
+            @Override
+            public void onReconnecting(@NonNull Room room, @NonNull TwilioException twilioException) {
+                videoStatusTextView.setText(String.format("Reconnecting to %s", room.getName()));
+            }
+
+            @Override
+            public void onReconnected(@NonNull Room room) {
+                videoStatusTextView.setText(String.format("Connected to %s", room.getName()));
+            }
+
+            @Override
+            public void onConnectFailure(Room room, TwilioException e) {
+                videoStatusTextView.setText("Failed to connect");
+//                configureAudio(false);
+//                intializeUI();
+            }
+
+            @Override
+            public void onDisconnected(Room room, TwilioException e) {
+                localParticipant = null;
+                videoStatusTextView.setText(String.format("Disconnected from %s", room.getName()));
+                room = null;
+                // Only reinitialize the UI if disconnect was not called from onDestroy()
+                if (!disconnectedFromOnDestroy) {
+                    //configureAudio(false);
+                    initializeVideoTextureViews();
+                    //intializeUI();
+                }
+            }
+
+            @Override
+            public void onParticipantConnected(Room room, RemoteParticipant remoteParticipant) {
+
+            }
+
+            @Override
+            public void onParticipantDisconnected(Room room, RemoteParticipant remoteParticipant) {
+            }
+
+            @Override
+            public void onRecordingStarted(Room room) {
+                /*
+                 * Indicates when media shared to a Room is being recorded. Note that
+                 * recording is only available in our Group Rooms developer preview.
+                 */
+                Log.d(TAG, "onRecordingStarted");
+            }
+
+            @Override
+            public void onRecordingStopped(Room room) {
+                /*
+                 * Indicates when media shared to a Room is no longer being recorded. Note that
+                 * recording is only available in our Group Rooms developer preview.
+                 */
+                Log.d(TAG, "onRecordingStopped");
+            }
+        };
+    }
 
 }
